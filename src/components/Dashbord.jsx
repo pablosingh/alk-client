ca98abea7100db17639cdefcb3d56c8c5adde128
Singh Pablo <pablo22_4@hotmail.com>
2022-10-14 15:20:33 -0300

    saldo

--- src/components/Dashbord.jsx
+++ src/components/Dashbord.jsx
@@ -41,7 +41,7 @@ const Dashbord = () => {
   return (
     <Container>
         <Card>
-            <h3>Saldo : {state.balance}</h3>
+            <h3 style={{color:"white"}}>Saldo : {state.balance}</h3>
             {/* <button onClick={ ()=> console.log(state) }>Estado</button> */}
             { state && state?.operations.map( o => <Operation 
                 id={o.id}
